be8c169d57a54ff4949dc4849b4fa6e2a9040f0f
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2023-08-01 15:13:19 +0500

    created functions add, subtract, multiply, power & factorial that passed all tests in TDD environment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(val1, val2) {
+return val1 + val2;
 };
 
-const subtract = function() {
-	
+const subtract = function(val1, val2) {
+	return val1 - val2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  if (arr.length === 0) {
+    return sum;
+  }
+  else {
+    let total = arr.reduce((sum, el) => sum + el);
+    return total;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(...arg) {
+  let multiplier = 1;
+  let result = arg.reduce((multiplier,el) => multiplier * el);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if (num === 0 || num === 1) {
+    return result;
+  } else {
+    for (let i = num; i >= 1; i--) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
