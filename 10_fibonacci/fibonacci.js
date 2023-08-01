deef8bdd5b67f83bb935787e6ea99e29f52a5fd8
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2023-08-01 15:59:36 +0500

    written code for creating fibonacci number to a number for which user ask and return that member of fibonacci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const fibonacciSeries = [1,1];
+    if (Number(index) < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= Number(index); i++) {
+            fibonacciSeries.push(fibonacciSeries[i-2]+fibonacciSeries[i-1]);
+        }
+        return fibonacciSeries[Number(index)-1];
+    }
 };
 
 // Do not edit below this line
