16803f9b916b55b4918d72759cf0959bedea8304
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2023-08-01 15:47:56 +0500

    written logic to check for words to be palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reverseStr = processStr.split("").reverse().join("");
+    return processStr == reverseStr;
 };
 
 // Do not edit below this line
